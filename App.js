3b3e6dfe326f7b4b41a21a4db24479ad71b22768
Douglas Lowder <douglowder@mac.com>
2022-09-02 18:41:00 -0700

    EAS build and update

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let us try an update one more time</Text>
       <StatusBar style="auto" />
     </View>
   );
